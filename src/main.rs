817a9368334819b76d642581fe390d4fbbf43e42
mohamedeliwa <mohamed.av11@gmail.com>
2023-01-02 22:08:26 +0200

    calculating the mode

--- src/main.rs
+++ src/main.rs
@@ -24,4 +24,35 @@ fn main() {
     } else {
         median = nums[((nums_count - 1) / 2)] as f32;
     }
+
+    // calculating the mode
+    let mut nums_repeating_count: HashMap<i32, i32> = HashMap::new();
+
+    // using a hashmap to record
+    // how many each item in nums is repeated
+    for num in &nums {
+        // if the num doesn't exist in the map we insert 0
+        // if it exists we return a mut reference to its value
+        // then increase the value by one
+        let count = nums_repeating_count.entry(*num).or_insert(0);
+        *count += 1;
+    }
+
+    // fetching the most repeated item (mode)
+    for (key, value) in &nums_repeating_count {
+        // if the value is bigger than the value of the current mode
+        // the mode is assigned the current key
+        // using unwrap_or(1) is to prevent using a num that 's only repeated once
+        if *value > nums_repeating_count.get(&mode).copied().unwrap_or(1) {
+            is_there_mode = true;
+            mode = *key;
+        }
+    }
+
+    println!("the median is: {median}");
+    if is_there_mode {
+        println!("the mode is: {mode}");
+    } else {
+        println!("there is no mode")
+    }
 }
